6ad34cdcbed3a2cf6e353c0227d07233ab2b7b8d
Mayank Muley <nitkkr.mayank@gmail.com>
2018-05-17 02:25:07 +0530

    done commitment

--- IMDB/src/main/java/com/RedPanda/testBase/TestBase_RedPanda.java
+++ IMDB/src/main/java/com/RedPanda/testBase/TestBase_RedPanda.java
@@ -6,11 +6,14 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import com.RedPanda.excelReader.Excel_Reader;
+
 public class TestBase_RedPanda{
 
 	public WebDriver driver;
 	String browser="firefox";
-	String BaseUrl="https://www.imdb.com/";
+	String BaseUrl="http://automationpractice.com/index.php";
+	Excel_Reader excel;
 
 	public void init() {
 		getBrowser(browser);
@@ -22,6 +25,8 @@ public class TestBase_RedPanda{
 	public void getBrowser(String browser) {
 		if(browser.equalsIgnoreCase("firefox")) {
 			//log.info("creating object of:"+browser);
+			System.setProperty("webdriver.gecko.driver", "E:\\geckodriver.exe");
+
 			driver=new FirefoxDriver();
 		}
 	}
@@ -31,6 +36,14 @@ public class TestBase_RedPanda{
 		driver.get(BaseUrl);
 		//driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS);
+	}
+	
+	public String[][] getData(String sheetName,String ExcelName) {
+		String path="C:\\Users\\User\\git\\Testing_Git\\IMDB\\src\\main\\java\\com\\RedPanda\\testData\\TestDataProvider.xlsx";
+		//String path=System.getProperty("user.dir")+"\\src\\main\\java\\com\\RedPanda\\testData\\"+ExcelName;
+		excel=new Excel_Reader(path);
+		String[][] data= excel.getDataFromSheet(sheetName,ExcelName);
+		return data;
 	}
 
 
